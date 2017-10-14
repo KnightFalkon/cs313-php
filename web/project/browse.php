ef77471a5812e40d93e79bda878efe0c9a87bb8b
KnightFalkon <jacobthehun@gmail.com>
2017-10-14 16:25:53 -0600

    testing things

--- web/project/browse.php
+++ web/project/browse.php
@@ -1,5 +1,8 @@
 <?php
   session_start();
+
+  require "dbConnect.php";
+  $db = get_db();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -39,18 +42,36 @@
   </head>
 
   <body>
-    <!-- <input type="text" id="input1">
-    <button id="button1">Click here for testing</button>
-    <input type="text" id="input2"> -->
 
     <h1 class="text-center title"> <span class="red">Rager</span>Gamer</h1>
 
     <hr class="thick">
 
+    <!-- <?php
+      $statement = $db->prepare("SELECT name, picture FROM games");
+      $statement->execute();
+      // Go through each result
+      while ($row = $statement->fetch(PDO::FETCH_ASSOC))
+      {
+        // The variable "row" now holds the complete record for that
+        // row, and we can access the different values based on their
+        // name
+        echo '<div class="col-xs-12 text-center">';
+        echo '<div class="row pad">';
+        echo '<img src="' . $row['picture'] . '" alt="' . $row['Title'] . '" height="270" width="480" class="img-responsive center-block">';
+        echo '<button class="btn-lg btn-primary" onclick="addToCart(' . $row['title'] . ')">Buy</button>';
+        echo '</div>';
+      }
+
+      echo '<div class="row pad">';
+      echo '<a href="cart3.php">Go To Cart</a>';
+      echo '</div>';
+    ?> -->
+
     <div class="col-xs-12 text-center">
       <div class="row pad">
         <img src="http://gameranx.com/wp-content/uploads/2017/03/Destiny-2-1080P-Wallpaper-1.jpg" alt="Destiny 2" height="270" width="480" class="img-responsive center-block">
-        <button class="btn-lg btn-primary" onclick="addToCart('Destiny 2')">Buy</button>
+        <button class="btn-lg btn-primary" onclick="addToCart(Destiny 2)">Buy</button>
       </div>
       <div class="row pad">
         <img src="http://www.logicalincrements.com/assets/img/articles/overwatch/maxresdefault.jpg" alt="OverWatch" height="270" width="480" class="img-responsive center-block">
