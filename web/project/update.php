9a63d4beffdb915fdb7e337e552fb4d5d0b09a4d
KnightFalkon <jacobthehun@gmail.com>
2017-10-26 13:31:52 -0600

    attempting to get signup page to work right

---
+++ web/project/update.php
@@ -0,0 +1,53 @@
+<?php
+
+session_start();
+
+require("dbConnect.php");
+$db = get_db();
+
+$query = 'SELECT * FROM users WHERE username=:username';
+$statement = $db->prepare($query);
+$statement->bindValue(':username', $_SESSION['username']);
+$result = $statement->execute();
+$userRow = $statement->fetch();
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+$name = test_input($_POST['name']);
+$street = test_input($_POST['street']);
+$city = test_input($_POST['city']);
+$state = test_input($_POST['state']);
+$zip = test_input($_POST['zip']);
+$cardNum = test_input($_POST['cardNum']);
+
+if (!isset($name) || $name == ""
+  || !isset($street) || $street == ""
+  || !isset($city) || $city == ""
+  || !isset($state) || $state == ""
+  || !isset($zip) || $zip == ""
+  || !isset($cardNum) || $cardNum == "")
+{
+  $_SESSION['error'] = "Try Again";
+  header("Location: updateInfo.php");
+	die(); 
+}
+
+$query = 'UPDATE users SET address = :address, city = :city, state = :state, zip = :zip, card_num = :card_num, name = :name WHERE username = :username';
+$statement = $db->prepare($query);
+$statement->bindValue(':username', $_SESSION['username']);
+$statement->bindValue(':address', $street);
+$statement->bindValue(':city', $city);
+$statement->bindValue(':state', $state);
+$statement->bindValue(':zip', $zip);
+$statement->bindValue(':card_num', $cardNum);
+$statement->bindValue(':name', $name);
+$statement->execute();
+
+header("Location: account.php");
+die(); 
+?>
--- web/project/updateInfo.php
+++ web/project/updateInfo.php
@@ -10,46 +10,6 @@ $statement = $db->prepare($query);
 $statement->bindValue(':username', $_SESSION['username']);
 $result = $statement->execute();
 $userRow = $statement->fetch();
-
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
-
-$name = test_input($_POST['name']);
-$street = test_input($_POST['street']);
-$city = test_input($_POST['city']);
-$state = test_input($_POST['state']);
-$zip = test_input($_POST['zip']);
-$cardNum = test_input($_POST['cardNum']);
-
-if (!isset($name) || $name == ""
-  || !isset($street) || $street == ""
-  || !isset($city) || $city == ""
-  || !isset($state) || $state == ""
-  || !isset($zip) || $zip == ""
-  || !isset($cardNum) || $cardNum == "")
-{
-  $_SESSION['error'] = "Try Again";
-  header("Location: updateInfo.php");
-	die(); 
-}
-
-$query = 'UPDATE users SET address = :address, city = :city, state = :state, zip = :zip, card_num = :card_num, name = :name WHERE username = :username';
-$statement = $db->prepare($query);
-$statement->bindValue(':username', $_SESSION['username']);
-$statement->bindValue(':address', $street);
-$statement->bindValue(':city', $city);
-$statement->bindValue(':state', $state);
-$statement->bindValue(':zip', $zip);
-$statement->bindValue(':card_num', $cardNum);
-$statement->bindValue(':name', $name);
-$statement->execute();
-
-header("Location: account.php");
-die(); 
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -84,7 +44,7 @@ die();
     ?>
     <div class="col-xs-12 text-center">    
       <h3>Update your information, so we can send you sweet games!</h3>
-      <form  action="updateInfo.php" method="post">
+      <form  action="update.php" method="post">
         <div class="row pad">
           <label for="username">username:</label>
           <input type="text" name="username" id="username" value="<?php $userRow['username']?>">
