3020259e3f96df3dfb01f664fe61ecf0fc8d6d52
KnightFalkon <jacobthehun@gmail.com>
2017-10-26 13:56:31 -0600

    attempting to get signup page to work right

--- web/project/browse.php
+++ web/project/browse.php
@@ -57,7 +57,7 @@
       <h1 class="text-center title"> <span class="red">Rager</span>Gamer</h1></br>
 
       <p class="text-right"><?php echo "Welcome " . $_SESSION['username']?></p>
-      <div class="center">
+      <div class="pull-right">
         <a href="account.php">Go to account</a>
         <a href="cart.php" class="text-right">Go to cart</a>
       </div>  
