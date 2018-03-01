34488925ab763b84458a00d71d27a3971385fac6
KnightFalkon <jacobthehun@gmail.com>
2018-03-01 16:54:24 -0700

    test

--- web/project/addItem.php
+++ web/project/addItem.php
@@ -14,7 +14,7 @@
       echo "This is the first line" . $_REQUEST[0];
 			print_r($_SESSION);
       foreach($_REQUEST as $value) {
-				$array = [$value, 0];
+				$array = array($value, 0);
 				if(in_array($value, $_SESSION)) {
 					$_SESSION[$value][1] = $_SESSION[$value][1] + 1;
 				}
