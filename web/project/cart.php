937c00d46f42d81225e54711684517db8fe94ed0
KnightFalkon <jacobthehun@gmail.com>
2018-03-01 16:13:46 -0700

    'ye'

--- web/project/cart.php
+++ web/project/cart.php
@@ -73,7 +73,7 @@
 					}
 					echo 'here we are';
 					//This is where things are added
-					$statement = $db->prepare("SELECT name, picture, description FROM games WHERE name = 'Destiny 2'");
+					$statement = $db->prepare("SELECT name, picture, description FROM games");
 					$statement->execute();
 					echo '<p> yes it got this far </p>';
 					// Go through each result
