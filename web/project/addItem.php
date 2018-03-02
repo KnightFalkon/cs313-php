c973adcfb7b473ff1fa7b2bd5f6a29139f056b5f
KnightFalkon <jacobthehun@gmail.com>
2018-03-01 17:12:49 -0700

    test

--- web/project/addItem.php
+++ web/project/addItem.php
@@ -14,10 +14,10 @@
       echo "This is the first line" . $_REQUEST[0];
 			print_r($_SESSION);
       foreach($_REQUEST as $value) {
-				$array = array($value, 0);
-				if(in_array($value, $_SESSION)) {
-					$_SESSION[$value][1] = "yes, this is working";
-					// $_SESSION[$value][1] = $_SESSION[$value][1] + 1;
+				$array = array($value, 1);
+				if($_SESSION[$value][0] == $value) {
+					// $_SESSION[$value][1] = "yes, this is working";
+					$_SESSION[$value][1] = $_SESSION[$value][1] + 1;
 				}
 				else {
 					$_SESSION[$value] = $array;					
