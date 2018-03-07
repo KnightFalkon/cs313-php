a44298985bdbaec9224dd608512cf9c5144ba965
KnightFalkon <jacobthehun@gmail.com>
2018-03-07 15:22:03 -0700

    finished

--- web/project/browse.php
+++ web/project/browse.php
@@ -83,7 +83,7 @@
 				echo '<hr class="thick">';
 				// echo '<p>' . $row['description'] . '</p>';
 				// echo $row['name']. $row['description'];				
-				echo '<div style="display:inline-block;"><img src="' . $row['picture'] . '" alt="' . $row['name'] . '" height="100" width="100" class="img-responsive center-block pull-left"></div>';
+				echo '<div class="pull-left" style="display:inline-block;"><img src="' . $row['picture'] . '" alt="' . $row['name'] . '" height="100" width="100" class="img-responsive center-block"></div>';
 				echo '<p>' . $row['description'] . '</p>';
         echo '<button class="btn-lg btn-primary" onclick="' . "addToCart('" . $row['name'] . "')" . '">Buy</button>' . "\n";
 				echo '</div>';
