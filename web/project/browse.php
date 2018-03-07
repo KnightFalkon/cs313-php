8f52e6fe38cf165849c243d5f11747ffbb2ad1c7
KnightFalkon <jacobthehun@gmail.com>
2018-03-07 15:50:43 -0700

    finished

--- web/project/browse.php
+++ web/project/browse.php
@@ -84,9 +84,9 @@
 				// echo '<p>' . $row['description'] . '</p>';
 				// echo $row['name']. $row['description'];				
 				echo '<div class="pull-left" style="display:inline-block;"><img src="' . $row['picture'] . '" alt="' . $row['name'] . '" height="150" width="200" class="img-responsive center-block">' . $row['esrb'] . '</div>';
-				echo '<p>'. $row['name'] . '</p>';
+				echo $row['name'];
 				echo '<hr class="thick">';
-				echo '<p>' . $row['description'] . '</p>';
+				echo $row['description'];
         echo '<div class="pull-right"><button class="btn-lg btn-primary" onclick="' . "addToCart('" . $row['name'] . "')" . '">Buy</button></div>';
 				echo '</div>';
 			}
